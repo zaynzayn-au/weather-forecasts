8b63c6450c63dd04f14a28d76cef98b389d8c8f0
Zayn Hwang <yz.huang1218@gmail.com>
2020-04-08 18:52:33 +1000

    增加阳间话语，格式调整

--- weather_fcst.py
+++ weather_fcst.py
@@ -8,7 +8,19 @@ def get_forecast():
 
 	if fcst.will_have_rain():
 		for f in fcst.when_rain():
-			print(f)
+			forecast_message += ('{} {}\n'.format(f.get_reference_time('iso'), f.get_status()))
+		forecast_message += '\n 要下雨啦，请不要带伞哦'
+	else:
+		forecast_message += '\n 接下来五天不会下雨，请带伞出门增加负重哦'
+	return forecast_message
+
+
 
 if __name__ == '__main__':
+	print('正在为您收集天气资料...')
 	get_forecast()
+
+	print('正在传送天气资料奥利给...')
+	send_email(msg)
+
+	Print('Done')
